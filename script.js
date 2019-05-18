4b9d31a0b1f4ee1e7986e2ee0c7352e4ac9881eb
Vesta080283 <43104215+Vesta080283@users.noreply.github.com>
2019-05-18 11:19:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 'use strict'
-let colors = ['red', 'orange', 'green', 'grey', 'magenta', 'blue', 'pink', 'brown', 'red', 'orange', 'green', 'grey', 'magenta', 'blue', 'pink', 'brown'];
+window.onload = () => {
+    let colors = ['red', 'orange', 'green', 'grey', 'magenta', 'blue', 'pink', 'brown', 'red', 'orange', 'green', 'grey', 'magenta', 'blue', 'pink', 'brown'];
+    /// or use HEX
+    //let colors = ['#FF0000', '#FF8000', '#FFFF00', '#40FF00', '#01DFD7', '#0000FF', '#FF00BF', '#071918', '#FF0000', '#FF8000', '#FFFF00', '#40FF00', '#01DFD7', '#0000FF', '#FF00BF', '#071918'];
+    
+    let play = false;
 
-/// may be use HEX
-//let colors = ['#FF0000', '#FF8000', '#FFFF00', '#40FF00', '#01DFD7', '#0000FF', '#FF00BF', '#071918', '#FF0000', '#FF8000', '#FFFF00', '#40FF00', '#01DFD7', '#0000FF', '#FF00BF', '#071918'];
+    document.body.onload = newField();
 
-let play = false;
-
-document.body.onload = newField();
-
-const newField = () => {    
+    function newField() {
         colors.sort(compareRandom);
-        colors.forEach( (color) => {
+        colors.forEach(function (color) {
             let borderElem = document.createElement('div');
             borderElem.className = 'border';
             let newElem = document.createElement('div');
@@ -20,89 +20,94 @@ const newField = () => {
             newElem.addEventListener('click', onClickCard);
             borderElem.appendChild(newElem);
             document.querySelector('.gameField').appendChild(borderElem);
-        })    
-}
+        })
+    }
 
-const compareRandom = (a, b) => {
-    return Math.random() - 0.5;
-}
+    function compareRandom(a, b) {
+        return Math.random() - 0.5;
+    }
 
-const clearField = () => {
-  let parent = document.querySelector('.gameField');
-  while (parent.firstChild) {
-    parent.firstChild.remove();
-    };
-}
+    function clearField() {
+        let parent = document.querySelector('.gameField');
+        while (parent.firstChild) {
+            parent.firstChild.remove();
+        };
+    }
 
-let check = false;
-let openCards = 0;
-let steps = 0;
-let selCard;
-let selColor;
+    let check = false;
+    let openCards = 0;
+    let steps = 0;
+    let selCard;
+    let selColor;
 
-const onClickCard = (e) => {
-    let card = e.target;
+    function onClickCard(e) {
+        let card = e.target;
 
-    if (play) {
-        if (getComputedStyle(card).opacity == 0) {
-            steps++;
-            card.style.opacity = 1;
-            setTimeout(() => {
-                if (check) {
-                    check = false;
-                    if (card.style.backgroundColor == selColor) {
-                        openCards++;
-                        if (openCards == 8) {
-                            alert('Вы выиграли! Затраченное время: ' + timer.innerHTML);
-                            resetTimer();
+        if (play) {
+            if (getComputedStyle(card).opacity == 0) {
+                steps++;
+                card.style.opacity = 1;
+
+                setTimeout(() => {
+                    if (check) {
+                        check = false;
+                        if (card.style.backgroundColor == selColor) {
+                            openCards++;
+                            if (openCards == 8) {
+                                alert('Вы выиграли! Затраченное время: ' + timer.innerHTML);
+                                resetTimer();
+                            }
+                        } else {
+
+                            selCard.style.opacity = 0;
+                            card.style.opacity = 0;
                         }
-                    } else {                        
-                        selCard.style.opacity = 0;
-                        card.style.opacity = 0;
+                    } else {
+                        selCard = card;
+                        selColor = card.style.backgroundColor;
+                        check = true;
                     }
-                } else {
-                    selCard = card;
-                    selColor = card.style.backgroundColor;
-                    check = true;
-                }
-            }, 100);
+                }, 100);
+            }
         }
     }
-}
 
-//description timer
+    //description timer
 
-let second = 0, minute = 0; hour = 0;
-let timer = document.querySelector('.timer');
-let interval;
+    let second = 0, minute = 0; hour = 0;
+    let timer = document.querySelector('.timer');
+    let interval;
 
-const startTimer = () => {
-    interval = setInterval( () => {
-        timer.innerHTML = minute + ":" + second;
-        second++;
-        if (second == 60) {
-            minute++;
-            second = 0;
-        }
-        if (minute == 60) {
-            hour++;
-            minute = 0;
-        }
-    }, 1000);
-}
+    function startTimer() {
+        interval = setInterval(function () {
+            timer.innerHTML = minute + ":" + second;
+            second++;
+            if (second == 60) {
+                minute++;
+                second = 0;
+            }
+            if (minute == 60) {
+                hour++;
+                minute = 0;
+            }
+        }, 1000);
+    }
 
-const resetTimer = () => {
-    second = 0;
-    minute = 0; 
-    hour = 0;
-    timer.innerHTML = "0:0";
-    clearInterval(interval);
-} 
+    function resetTimer() {
+        second = 0;
+        minute = 0;
+        hour = 0;
+        timer.innerHTML = "0:0";
+        clearInterval(interval);
+    }
 
-const startGame = (_play) => {
-    play = _play;
-    resetTimer();
-    clearField();    
-    newField();    
-    startTimer();
+    document.querySelector('.startBtn').addEventListener('click', startGame);
+
+    function startGame() {
+        play = true;
+        resetTimer();
+        clearField();
+        newField();
+        startTimer();
+    }
 }
